1539ed4c1518983506f07711e038d3bcc034c24c
litt1e-p <free9fw@gmail.com>
2015-12-22 09:52:09 +0800

    add screen constant

---
+++ ScreenConstant.h
@@ -0,0 +1,18 @@
+//
+//  ScreenConstant.h
+//
+//  Created by litt1e-p on 15/12/22.
+//  Copyright © 2015年 litt1e-p. All rights reserved.
+//
+
+#ifndef ScreenConstant_h
+#define ScreenConstant_h
+
+#define ScreenWidth [UIScreen mainScreen].bounds.size.width
+#define ScreenHeight [UIScreen mainScreen].bounds.size.height
+#define ScreenInch3p5 ([UIScreen mainScreen].bounds.size.height == 480)
+#define ScreenInch4p ([UIScreen mainScreen].bounds.size.height == 568)
+#define ScreenInch4p7 ([UIScreen mainScreen].bounds.size.height == 667)
+#define ScreenInch5p5 ([UIScreen mainScreen].bounds.size.height == 1104)
+
+#endif /* ScreenConstant_h */
